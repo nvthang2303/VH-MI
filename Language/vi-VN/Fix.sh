8373966c719f7f28b436a77c9e6b40f6afa17b13
kakathic <kakathic@gmail.com>
2022-01-05 16:15:00 +0700

    Update Fix.sh

--- Language/vi-VN/Fix.sh
+++ Language/vi-VN/Fix.sh
@@ -9,3 +9,11 @@ Test=Kakathic
 if [ "$pkg" == "com.android.systemui" ];then
 sed -i -e 's|<string name="status_bar_clock_date_weekday_format">EEE, d MMM</string>|<string name="status_bar_clock_date_weekday_format">E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_weekday_format_12">EEE, d MMM</string>|<string name="status_bar_clock_date_weekday_format_12">a E, dd - MM</string>|g' -e 's|<string name="status_bar_clock_date_time_format">H:mm E, d MMM</string>|<string name="status_bar_clock_date_time_format">H:mm - E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_time_format_12">h:mm aa, E, d MMM</string>|<string name="status_bar_clock_date_time_format_12">h:mm a - E, dd.MM - (e.N)</string>|g' -e 's|<string name="kilobyte_per_second">KB/s</string>|<string name="megabyte_per_second">M/s </string>|g' -e 's|<string name="megabyte_per_second">MB/s</string>|<string name="kilobyte_per_second">K/s </string>|g' -e 's|<string name="status_bar_clock_date_format">E, d MMM</string>|<string name="status_bar_clock_date_format">E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_format_12">E, d MMM</string>|<string name="status_bar_clock_date_format_12">a E, dd - MM</string>|g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
 fi
+
+if [ "$pkg" == "com.miui.securitycenter" ];then
+sed -i 's|`||g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
+fi
+
+if [ "$pkg" == "com.android.browser" ];then
+sed -i 's|>>|>|g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
+fi
