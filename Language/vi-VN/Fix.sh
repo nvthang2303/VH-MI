d239ab1404f1df838f5b48462e1d8f0f88a7a993
kakathic <kakathic@gmail.com>
2022-01-15 20:48:01 +0700

    Update Fix.sh

--- Language/vi-VN/Fix.sh
+++ Language/vi-VN/Fix.sh
@@ -12,8 +12,7 @@ fi
 
 if [ "$pkg" == "com.miui.securitycenter" ];then
 sed -i 's|`||g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
-sed -i 's|once_settings_title|wdcvh123|' /data/tools/tmp/*/*/main/$path/res/*/*.xml
-
+sed -i "s/once_settings_title/wdcvh123/" /data/tools/tmp/*/*/main/$path/res/*/*.xml
 fi
 
 if [ "$pkg" == "com.android.browser" ];then
