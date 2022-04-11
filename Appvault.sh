cd9fd680d78f8c04fb541db7f1b58feeaae03afb
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-04-11 15:29:30 +0700

    1.4

--- Appvault.sh
+++ Appvault.sh
@@ -1,2 +1,24 @@
 Test=kakathic
 
+# KAKATHIC
+
+Caidat () {
+if [ ! -e /data/data/com.miui.personalassistant/files/maml/res/Widget$1 ];then
+echo "- Đang tải dữ liệu."
+Taive "https://github.com/kakathic/VH-MI/releases/download/Widgets/Widget$1.zip" "/data/local/tmp/Appvault/Test.zip"
+echo "- Cài đặt..."
+unzip -qo /data/local/tmp/Appvault/Test.zip -d /data/data/com.miui.personalassistant/files/maml/res
+fi
+}
+
+Caidat2 () {
+if [ ! -e /data/data/com.miui.personalassistant/files/maml/res/0/Widget$1 ];then
+echo "- Đang tải dữ liệu."
+Taive "https://github.com/kakathic/VH-MI/releases/download/Widgets/Widget$1.zip" "/data/local/tmp/Appvault/Test.zip"
+echo "- Cài đặt..."
+unzip -qo /data/local/tmp/Appvault/Test.zip -d /data/data/com.miui.personalassistant/files/maml/res/0
+fi
+}
+
+Caidat 1
+
