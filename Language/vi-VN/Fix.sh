3f86776d679470b535b9ea3e661baca358219481
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-04-16 07:02:12 +0700

    Update Fix.sh

--- Language/vi-VN/Fix.sh
+++ Language/vi-VN/Fix.sh
@@ -1,13 +1,8 @@
-# KAKATHIC
-
-
-Test=Kakathic
-
-
+# Kakathic
 # Thêm lịch âm và sửa đổi bộ đếm dữ liệu
 
 if [ "$pkg" == "com.android.systemui" ];then
-sed -i -e 's|<string name="status_bar_clock_date_weekday_format">EEE, d MMM</string>|<string name="status_bar_clock_date_weekday_format">E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_weekday_format_12">EEE, d MMM</string>|<string name="status_bar_clock_date_weekday_format_12">a E, dd - MM</string>|g' -e 's|<string name="status_bar_clock_date_time_format">H:mm E, d MMM</string>|<string name="status_bar_clock_date_time_format">H:mm - E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_time_format_12">h:mm aa, E, d MMM</string>|<string name="status_bar_clock_date_time_format_12">h:mm a - E, dd.MM - (e.N)</string>|g' -e 's|<string name="kilobyte_per_second">KB/s</string>|<string name="megabyte_per_second">M/s </string>|g' -e 's|<string name="megabyte_per_second">MB/s</string>|<string name="kilobyte_per_second">K/s </string>|g' -e 's|<string name="status_bar_clock_date_format">E, d MMM</string>|<string name="status_bar_clock_date_format">E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_format_12">E, d MMM</string>|<string name="status_bar_clock_date_format_12">a E, dd - MM</string>|g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
+sed -i -e 's|<string name="status_bar_clock_date_weekday_format">EEE, d MMM</string>|<string name="status_bar_clock_date_weekday_format">E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_weekday_format_12">EEE, d MMM</string>|<string name="status_bar_clock_date_weekday_format_12">a E, dd - MM</string>|g' -e 's|<string name="status_bar_clock_date_time_format">H:mm E, d MMM</string>|<string name="status_bar_clock_date_time_format">H:mm - E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_time_format_12">h:mm aa, E, d MMM</string>|<string name="status_bar_clock_date_time_format_12">h:mm a - E, dd.MM - (e.N)</string>|g' -e 's|<string name="kilobyte_per_second">KB/s</string>|<string name="megabyte_per_second">M/s </string>|g' -e 's|<string name="megabyte_per_second">MB/s</string>|<string name="kilobyte_per_second">K/s </string>|g' -e 's|<string name="status_bar_clock_date_format">E, d MMM</string>|<string name="status_bar_clock_date_format">E, dd.MM - (e.N)</string>|g' -e 's|<string name="status_bar_clock_date_format_12">E, d MMM</string>|<string name="status_bar_clock_date_format_12">a E, dd - MM</string>|g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
 fi
 
 if [ "$pkg" == "com.miui.securitycenter" ];then
@@ -23,3 +18,10 @@ if [ "$pkg" == "com.android.settings" ];then
 rm -fr /data/tools/tmp/*/*/main/$path/res/values-mcc460*
 fi
 
+if [ "$pkg" == "com.android.calendar" ];then
+sed -i -e 's|: )||g' -e 's|: (||g' -e 's|<string name="edit_event_reminder_summary_3_days_before">Trước %2$d ngày và vào ngày đó lúc %1$s</string>|<string name="edit_event_reminder_summary_3_days_before">Trước 3 ngày và vào ngày đó lúc %1$s</string>|g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
+fi
+
+if [ "$pkg" == "com.miui.home" ];then
+sed -i 's|<string name="system_shortcuts_more_operation">Thông tin ứng dụng</string>|<string name="system_shortcuts_more_operation">Thông tin</string>|g' /data/tools/tmp/*/*/main/$path/res/*/*.xml
+fi
