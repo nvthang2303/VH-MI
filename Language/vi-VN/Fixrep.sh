aae5d786196682d693e7eaa6d798688e457d5479
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-07-24 14:23:03 +0700

    Add files via upload

--- Language/vi-VN/Fixrep.sh
+++ Language/vi-VN/Fixrep.sh
@@ -3,27 +3,31 @@ apktool b /data/tools/tmp/*/*/main/$path -f -o "/data/tools/tmp/Apk/tmp/Z.$pkg.a
 mkdir -p "$MPATH$Overlay"
 apksign "/data/tools/tmp/Apk/tmp/Z.$pkg.apk" "$MPATH$Overlay/Z.$pkg.apk"
 rm -fr "$MPATH$Overlay/Z.$pkg.apk.idsig"
-}
-RepapkF
 
+}
+
+RepapkF
 
 # Trùng string
-while true; do
 if [ "$(grep -cm1 'is already defined.' /data/tools/tmp/Fix.txt)" == 1 ];then
-Sotk=$(grep -m1 'is already defined.' /data/tools/tmp/Fix.txt | cut -d : -f3)
-sed -i ''$Sotk'd' $Park/res/*/*.xml
-sed -i '/'$Sotk'/d' /data/tools/tmp/Fix.txt
-fi
-[ "$(grep -c 'is already defined.' /data/tools/tmp/Fix.txt)" == 0 ] && break
+while true; do
+Linktk=$(grep -m1 'is already defined.' /data/tools/tmp/Fix.txt | cut -d : -f2)
+Vbtk=$(grep -m1 'is already defined.' /data/tools/tmp/Fix.txt | awk '{print $6}')
+sotk=$(grep -nm1 "$Vbtk" $Linktk | cut -d : -f1)
+sed -i ''$sotk'd' $Linktk
+sed -i '/'$Vbtk'/d' /data/tools/tmp/Fix.txt
+[ "$(grep -cm1 'is already defined.' /data/tools/tmp/Fix.txt)" == 0 ] && break
 done
-
-
-
-
-
-
-
-
-
-
 RepapkF
+fi
+
+
+
+
+
+
+
+
+
+
+
