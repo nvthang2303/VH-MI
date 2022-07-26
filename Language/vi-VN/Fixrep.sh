0ed62a53df0902c42a084f41d9d9f19057fa766d
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-07-26 21:59:55 +0700

    Update Fixrep.sh

--- Language/vi-VN/Fixrep.sh
+++ Language/vi-VN/Fixrep.sh
@@ -23,14 +23,14 @@ done
 RepapkF
 fi
 
-# String lỗi
+# String lỗi thiếu ký tự
 if [ "$(grep -cm1 'invalid token' /data/tools/tmp/$pkg.txt)" == 1 ];then
 while true; do
+jtt5uu=$(grep -m1 'invalid token' /data/tools/tmp/$pkg.txt | cut -d : -f2)
 vrgrh=$(grep -m1 'invalid token' /data/tools/tmp/$pkg.txt | cut -d : -f3)
-sed -i ''$vrgrh'd' $Park/res/*/strings.xml
+sed -i "/$vrgrh/c \ " $jtt5uu
 sed -i '/'$vrgrh'/d' /data/tools/tmp/$pkg.txt
 [ "$(grep -cm1 'invalid token' /data/tools/tmp/$pkg.txt)" == 0 ] && break
 done
 RepapkF
 fi
-
