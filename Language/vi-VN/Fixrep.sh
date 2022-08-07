2c764bd036e5d0e7d7c703fbf6f6a2085fcb573c
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-08 00:42:16 +0700

    Update Fixrep.sh

--- Language/vi-VN/Fixrep.sh
+++ Language/vi-VN/Fixrep.sh
@@ -1,25 +1,20 @@
-
 RepapkF () {
-apktool b /data/tools/tmp/*/*/main/$path -f -o "/data/tools/tmp/Apk/tmp/Z.$pkg.apk" 2>>/data/tools/tmp/$pkg.txt >>/data/tools/tmp/$pkg.txt
-[ -e /data/tools/$pkg.txt ] || cp -rf /data/tools/tmp/$pkg.txt /data/tools/$pkg.txt
-mkdir -p "$MPATH$Overlay"
-apksign "/data/tools/tmp/Apk/tmp/Z.$pkg.apk" "$MPATH$Overlay/Z.$pkg.apk"
-rm -fr "$MPATH$Overlay/Z.$pkg.apk.idsig"
-
+apktool b $TMPDIR/rac/*/*/main/$path -f -o "$TMPDIR/rac/Apk/tmp/Z.$pkg.apk" 2>> $TMPDIR/rac/$pkg.txt >> $TMPDIR/rac/$pkg.txt
+[ -e $TMPDIR/rac/$pkg.txt ] && cp -rf $TMPDIR/rac/$pkg.txt $Dtool/error/$pkg.txt
+apksign "$TMPDIR/rac/Apk/Z.$pkg.apk" "$MPATH$Overlay/Z.$pkg.apk" 2>/dev/null >/dev/null
 }
 
 RepapkF
 
 # Trùng string
-if [ "$(grep -cm1 'is already defined.' /data/tools/tmp/$pkg.txt)" == 1 ];then
+if [ "$(grep -cm1 'is already defined.' $TMPDIR/rac/$pkg.txt)" == 1 ];then
 while true; do
-Linktk=$(grep -m1 'is already defined.' /data/tools/tmp/$pkg.txt | cut -d : -f2)
-Vbtk=$(grep -m1 'is already defined.' /data/tools/tmp/$pkg.txt | awk '{print $6}')
+Linktk=$(grep -m1 'is already defined.' $TMPDIR/rac/$pkg.txt | cut -d : -f2)
+Vbtk=$(grep -m1 'is already defined.' $TMPDIR/rac/$pkg.txt | awk '{print $6}')
 sotk=$(grep -nm1 "$Vbtk" $Linktk | cut -d : -f1)
 sed -i ''$sotk'd' $Linktk
-sed -i '/'$Vbtk'/d' /data/tools/tmp/$pkg.txt
-[ "$(grep -cm1 'is already defined.' /data/tools/tmp/$pkg.txt)" == 0 ] && break
+sed -i '/'$Vbtk'/d' $TMPDIR/rac/$pkg.txt
+[ "$(grep -cm1 'is already defined.' $TMPDIR/rac/$pkg.txt)" == 0 ] && break
 done
 RepapkF
 fi
-
